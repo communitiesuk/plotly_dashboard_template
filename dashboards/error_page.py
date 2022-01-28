d2cb874780588a62f0b6c773bdfb35804c6fd962
Rhys <Rhys.Jones@madetech.com>
2022-01-28 12:28:58 +0000

    Add relevant files for creating a dashboard
    Copied across files that are generic to making dashboards from housing data dashboards.

---
+++ dashboards/error_page.py
@@ -0,0 +1,52 @@
+"""error"""
+from dash import html
+
+from components.main_content import main_content
+
+
+def error_page():
+    """Return a layout showing an error message and a way to contact the Data Dashboards team"""
+    data_dashboards_teams_link = (
+        "https://teams.microsoft.com/l/team/19"
+        "%3aJlkCtA_axw4AtAcLorJqYzHumBGr6cLw-s4hp5hyUQ81%40thread.tacv2"
+        "/conversations?groupId=7d4f2fda-52d8-444c-a8cc-9e66e83dcb9d"
+        "&tenantId=bf346810-9c7d-43de-a872-24a2ef3995a8 "
+    )
+
+    return main_content(
+        [
+            html.Div(
+                [
+                    html.H1(
+                        "Sorry, there was a problem with that request",
+                        className="govuk-heading-l",
+                    ),
+                    html.P(
+                        [
+                            "Try refreshing the page, or going back to ",
+                            html.A(
+                                "the dashboard homepage",
+                                href="/",
+                                className="govuk-link",
+                            ),
+                            ".",
+                        ],
+                        className="govuk-body",
+                    ),
+                    html.P(
+                        [
+                            "Contact the Data Dashboards project team on ",
+                            html.A(
+                                "Microsoft Teams",
+                                href=data_dashboards_teams_link,
+                                className="govuk-link",
+                            ),
+                            " if this keeps happening.",
+                        ],
+                        className="govuk-body",
+                    ),
+                ],
+                style={"align-self": "center"},
+            )
+        ]
+    )
---
+++ index.py
@@ -0,0 +1,85 @@
+"""
+Create paths to serve different dashboards.  Add new paths in the display_page callback.
+"""
+import logging
+import os
+
+from dash import dcc, html, Input, Output
+
+from app import app
+from components.banners import message_banner
+from components.dashboard_container import dashboard_container
+from components.header import header
+from dashboards.error_page import error_page
+from dashboards.template_dashboard import template_dashboard
+from lib.url import dict_to_query_string
+
+app.title = "Template Dashboard"
+
+app.layout = html.Div(
+    [
+        header(app.title),
+        html.Div(
+            [
+                html.Div(
+                    [
+                        dcc.Location(id="url", refresh=False),
+                        message_banner(
+                            category="UPDATE",
+                            message="This is an example update banner"
+                        ),
+                        html.Div(id="page-content"),
+                    ],
+                    className="govuk-main-wrapper--auto-spacing govuk-!-padding-top-2",
+                )
+            ],
+            className="govuk-width-container",
+        ),
+    ]
+)
+
+
+@app.callback(
+    Output("protective-marking", "children"),
+    Output("page-content", "children"),
+    Input("url", "pathname"),
+    Input("url", "search"),
+)
+def display_page(pathname, query_string):
+    """Show the user the correct dashboard for the given path"""
+    try:
+        paths = {
+            "/": {
+                "protective_marking": "OFFICIAL",
+                "dashboard": lambda: template_dashboard(),
+            }
+        }
+
+        for key, route in paths.items():
+            if pathname == key:
+                return [
+                    route["protective_marking"],
+                    dashboard_container(route["dashboard"]()),
+                ]
+    except Exception as exception:
+        if os.environ.get("STAGE") == "production":
+            logging.exception(exception)
+            return ["OFFICIAL", dashboard_container(error_page())]
+
+        raise exception
+
+    page_not_found = "404"
+    return page_not_found
+
+
+@app.callback(
+    Output("url", "search"),
+    Input("metric", "value")
+)
+def update_url(
+    metric
+):
+    """When the user changes any filter panel elements, update the URL query parameters"""
+    return dict_to_query_string(
+        metric=metric
+    )
