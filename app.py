315c176f5f8a3ec1dc6ba878951bd4b14b9c9326
Kate Riley <kate.riley@communities.gov.uk>
2022-08-23 15:00:44 +0100

    include package imports and update package version number

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ Data visualizations for the Department of Levelling Up, Housing and Communities
 
 import dash
 from gov_uk_dashboards.template import read_template
+from pkg_gov_uk_dashboards.lib.http_headers import setup_application_http_response_headers
+from pkg_gov_uk_dashboards.lib.logging import configure_logging
 import bootstrap
-from configuration import logging
 
-logging.configure_logging()
+configure_logging()
 
 app = dash.Dash(__name__, update_title=None)
 app.config.suppress_callback_exceptions = True
@@ -15,4 +16,4 @@ app.index_string = read_template()
 
 server = app.server
 
-bootstrap.setup_application_http_response_headers(app)
+setup_application_http_response_headers(app)
