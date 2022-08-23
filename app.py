f95c4b96f96520ebf6d3d3914ca118e85f1b1aeb
Kate Riley <kate.riley@communities.gov.uk>
2022-08-23 11:47:45 +0100

    update files to be similar to other dashboards

--- app.py
+++ app.py
@@ -4,9 +4,15 @@ Data visualizations for the Department of Levelling Up, Housing and Communities
 
 import dash
 from gov_uk_dashboards.template import read_template
+import bootstrap
+from configuration import logging
 
-app = dash.Dash(__name__, suppress_callback_exceptions=True)
+logging.configure_logging()
 
+app = dash.Dash(__name__, update_title=None)
+app.config.suppress_callback_exceptions = True
 app.index_string = read_template()
 
 server = app.server
+
+bootstrap.setup_application_http_response_headers(app)
