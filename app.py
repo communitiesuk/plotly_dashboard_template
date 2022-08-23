2b6f53a02da2611d36e77c4220476a7a07411f26
Kate Riley <kate.riley@communities.gov.uk>
2022-08-23 16:20:58 +0100

    update template

--- app.py
+++ app.py
@@ -4,9 +4,8 @@ Data visualizations for the Department of Levelling Up, Housing and Communities
 
 import dash
 from gov_uk_dashboards.template import read_template
-from pkg_gov_uk_dashboards.lib.http_headers import setup_application_http_response_headers
-from pkg_gov_uk_dashboards.lib.logging import configure_logging
-import bootstrap
+from gov_uk_dashboards.lib.http_headers import setup_application_http_response_headers
+from gov_uk_dashboards.lib.logging import configure_logging
 
 configure_logging()
 
