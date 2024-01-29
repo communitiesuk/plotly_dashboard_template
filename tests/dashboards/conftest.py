46e5a18cd0cc10bc60b4baa493509a0112f07610
tom-clarke-dluhc <tom.clarke@levellingup.gov.uk>
2024-01-29 10:00:56 +0000

    Add conftest for integration test to work

---
+++ tests/dashboards/conftest.py
@@ -0,0 +1,11 @@
+"""Pytest automatically loads this file before executing tests"""
+# pylint: disable=unused-import
+from tests.plugins import (
+    data_folder_location,
+    setup_chromedriver_for_browser_tests,
+    setup_proxy_for_browser_tests,
+    get_dashboard_utils,
+    chromedriver,
+)
+
+# pylint: enable=unused-import
