ad1f8f901b08ca4ee8862f7b9cfc6aa2f5c8a60e
Kate Riley <kate.riley@communities.gov.uk>
2025-07-18 14:24:40 +0100

    wip

--- tests/data_tests/conftest.py
+++ tests/data_tests/conftest.py
@@ -1,6 +1,6 @@
 """Pytest automatically loads this file before executing tests"""
 
 # pylint: disable=unused-import
-from tests.plugins import data_tests_folder_location, cleanup_dash_duo_driver
+from tests.plugins import data_tests_folder_location
 
 # pylint: enable=unused-import
